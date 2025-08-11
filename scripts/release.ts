d8eefe8c599802dc8c158c1935ba64fd9fb2e095
nivo0616 <nikita.vorobev@netcracker.com>
2025-08-11 09:59:27 +0300

    Add gitignore

--- scripts/release.ts
+++ scripts/release.ts
@@ -2,7 +2,7 @@ import {getValueByFlag, hasFlag} from "./shared/argv.utils";
 import {ReleaseMode} from "./shared/release-mode";
 import {bumpVersion} from "./shared/bump-version";
 import {version} from '../package.json';
-import {runStandardVersion} from "./shared/run-standard-version";
+// import {runStandardVersion} from "./shared/run-standard-version";
 
 const ci = hasFlag('--ci-mode');
 const mode = getValueByFlag<ReleaseMode>('--release-as', 'minor');
@@ -14,7 +14,7 @@ console.log(JSON.stringify({ci, mode, newVersion, dryRun}, null, 4));
 
 (async function main(): Promise<void> {
     if (ci) {
-        runStandardVersion(newVersion, dryRun);
+        // runStandardVersion(newVersion, dryRun);
     } else {
         //TODO
     }
